ff0c48e72213236edfb4e8a0bfc6275533a39349
boguszpawlowski <bogusz.pawlowski@gmail.com>
2021-05-03 10:56:39 +0200

    Add animation to the calendar size

--- sample/src/main/java/com/bpawlowski/composecalendar/sample/MainActivity.kt
+++ sample/src/main/java/com/bpawlowski/composecalendar/sample/MainActivity.kt
@@ -3,11 +3,13 @@ package com.bpawlowski.composecalendar.sample
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
@@ -17,9 +19,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.bpawlowski.composecalendar.Calendar
-import com.bpawlowski.composecalendar.CalendarState
 import com.bpawlowski.composecalendar.rememberCalendarState
 import com.bpawlowski.composecalendar.selection.SelectionMode
+import com.bpawlowski.composecalendar.selection.SelectionState
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,22 +39,26 @@ fun MainScreen() {
     Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
       val calendarState = rememberCalendarState()
 
-      Calendar(calendarState = calendarState)
+      Calendar(
+        calendarState = calendarState,
+        modifier = Modifier
+          .padding(8.dp)
+          .animateContentSize()
+      )
 
-      CalendarControls(calendarState = calendarState)
+      SelectionControls(selectionState = calendarState.selectionState)
     }
   }
 }
 
 @Composable
-fun CalendarControls(
-  calendarState: CalendarState,
+fun SelectionControls(
+  selectionState: SelectionState,
 ) {
   Text(
     text = "Calendar Selection Mode",
     style = MaterialTheme.typography.h5,
   )
-  val selectionState = calendarState.selectionState
   SelectionMode.values().forEach { selectionMode ->
     Row(modifier = Modifier.fillMaxWidth()) {
       RadioButton(
