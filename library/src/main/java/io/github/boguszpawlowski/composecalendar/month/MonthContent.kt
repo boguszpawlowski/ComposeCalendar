f89c3b33a811a450f46e0352b20ddbb315d713c1
boguszpawlowski <bogusz.pawlowski@gmail.com>
2022-05-01 21:15:37 +0200

    Adjust sample to the new functionality

--- library/src/main/java/io/github/boguszpawlowski/composecalendar/month/MonthContent.kt
+++ library/src/main/java/io/github/boguszpawlowski/composecalendar/month/MonthContent.kt
@@ -8,18 +8,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material.rememberSwipeableState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.testTag
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
@@ -53,7 +46,6 @@ internal fun <T : SelectionState> MonthPager(
   val startIndex = PagerItemCount / 2
   val pagerState = rememberPagerState(initialPage = startIndex)
   val coroutineScope = rememberCoroutineScope()
-  var pagerHeight by remember { mutableStateOf(0) }
 
   val monthPagerState = remember {
     MonthPagerState(
@@ -69,22 +61,7 @@ internal fun <T : SelectionState> MonthPager(
     state = pagerState,
     verticalAlignment = Alignment.Top,
   ) { index ->
-    var pageHeight by remember { mutableStateOf(0) }
-
     MonthContent(
-      modifier = Modifier
-        .layout { measurable, constraints ->
-          val placeable = measurable.measure(constraints)
-          pageHeight = placeable.height
-          layout(constraints.maxWidth, pagerHeight) {
-            placeable.placeRelative(0, 0)
-          }
-        }
-        .onGloballyPositioned {
-          if (it.positionInParent().x == 0f) {
-            pagerHeight = pageHeight
-          }
-        },
       showAdjacentMonths = showAdjacentMonths,
       selectionState = selectionState,
       currentMonth = monthPagerState.getMonthForPage(index.toIndex()),
